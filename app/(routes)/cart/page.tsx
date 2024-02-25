a3b9f4005b6af944ee509d86869c95d60f8c031b
Thien Tran <thienforever2211@gmail.com>
2024-02-25 15:06:38 +0700

    fix hygration error

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -8,7 +8,17 @@ import CartItem from "./components/cart-item";
 import Summary from "./components/summary";
 
 const CartPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
   const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   
   return (
     <div className="bg-white">
